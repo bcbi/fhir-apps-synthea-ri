f25664b1ceb56edf7c7044b9dc9c83cf30ec55b1
Dilum Aluthge <dilum@aluthge.com>
2024-04-01 16:23:00 -0400

    Read the Flask port number from an environment variable

--- src/fhir_condition_finder_synthea_ri.py
+++ src/fhir_condition_finder_synthea_ri.py
@@ -17,7 +17,7 @@ password = os.getenv("FHIR_PASSWORD")
 def request_patient(patient_id, credentials):
 
     req = requests.get(FHIR_SERVER_BASE_URL + "/Patient/" + str(patient_id), auth = credentials)
-    
+
     print(f"Requests status: {req.status_code}")
 
     response = req.json()
@@ -36,7 +36,7 @@ def search_patients_by_condition(condition_id, credentials):
         patients = [request_patient(patient_id, credentials) for patient_id in patient_ids]
          # Convert patient details into set of tuples to ensure uniqueness
         unique_patients = set((patient['id'], patient['name'][0]['given'][0], patient['name'][0]['family'], patient['gender'], patient['birthDate']) for patient in patients)
-        
+
         total_patients = len(unique_patients)
         return {'unique_patients': unique_patients, 'total_patients': total_patients}
     else:
@@ -62,5 +62,6 @@ def index_eu():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port_str = os.environ['BCBI_MY_FHIR_PORT']
+    port_int = int(port_str)
+    app.run(debug=True, port=port_int)
